ffe5efd58b1753804961129a082767b5ab33ce00
antonmamchur <toxicusdraw@gmail.com>
2023-02-01 17:39:34 +0600

    pizza-block selectors

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
-import './scss/app.scss'
-import {Header} from './components/header'
-import {Categories} from "./components/categories";
-import {Sort} from "./components/sort"
-import {PizzaBlock} from "./components/pizza-block";
+import "./scss/app.scss";
+import { Header } from "./components/header";
+import { Categories } from "./components/categories";
+import { Sort } from "./components/sort";
+import { PizzaBlock } from "./components/pizza-block";
+
+import pizzas from "./assets/pizzas.json";
 
 function App() {
-    return (
-        <div className="App">
-            <div className="wrapper">
-                <Header/>
-                <div className="content">
-                    <div className="container">
-                        <div className="content__top">
-                            <Categories/>
-                            <Sort/>
-                        </div>
-                        <h2 className="content__title">Все пиццы</h2>
-                        <div className="content__items">
-                            <PizzaBlock title='Мексиканская' price={345}/>
-                            <PizzaBlock/>
-                            <PizzaBlock/>
-                            <PizzaBlock/>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
             </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              {pizzas.map((obj) => (
+                <PizzaBlock image={obj.imageUrl} {...obj} />
+              ))}
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 
 export function Categories() {
-    const list = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
-    const [active, setActive] = React.useState(0)
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
 
-    const handleActive = (index) => {
-        setActive(index)
-    }
+  const [active, setActive] = React.useState(0);
 
-    return (<div className="categories">
-        <ul>
-            {list.map((item, index) => <li onClick={() => handleActive(index)} key={item}
-                                           className={active === index ? 'active' : ''}>{item}</li>)}
-        </ul>
-    </div>)
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((category, index) => (
+          <li
+            key={index}
+            onClick={() => setActive(index)}
+            className={active === index ? "active" : ""}
+          >
+            {category}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
